d0227084f5b0bc644a55535b540fea4dc3849817
shreyancode <shresthashreyan5@gmail.com>
2025-06-23 13:00:29 +0545

    working fine

--- backend/displayForms.php
+++ backend/displayForms.php
@@ -2,8 +2,17 @@
 session_start();
 include_once("./connectToDb.php");
 $conn=connect();
+if(!isset($_GET['event_id'])){
+    die("id missing");
+}
+$event_id=$_GET['event_id'];
+$result=$conn->query("select title,description,date,time,location from events where id='event_id' and status='approved'");
+if($result->num_rows === 0)
+{
+    die("event not found or not approved");
 
-
+}
+$eventRes=$event->fetch_assoc();
 
 
 ?>
